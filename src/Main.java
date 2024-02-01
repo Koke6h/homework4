22f386f392ba9fc4fbe5e6d70c4177e21efc98f5
User <git@github.com:Koke6h/helloworld.git>
2024-02-01 22:05:53 +0400

    commit hw4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,82 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+        System.out.println("Домашняя работа");
+        System.out.println("Здача 1");
+        int age = 18;
+        System.out.println("Если человек возраст равен=" + age);
+        if (age >= 18) {
+            System.out.println("то, он совершенолетен");
+        } else {
+            System.out.println("Он не достиг совершенолетия");
+        }
+        System.out.println("Задача 2");
+        byte temperature = 6;
+        System.out.println("На улице" + temperature + "градусов");
+        if (temperature < 5) {
+            System.out.println("На улице холодно, нужно надеть шапку");
+        } else {
+            System.out.println("Сегодня тепло, можно идти без шапки");
+        }
+        System.out.println("Задача 3");
+        int speed = 40;
+        System.out.println("Если скорость" + speed + ", то");
+        if (speed > 60 && speed < 60) {
+            System.out.println("Придется заплатить штраф");
+        } else {
+            System.out.println("Можно ездить спокойно");
+        }
+        System.out.println("Задача 4");
+        int ageMe = 22;
+        System.out.println("Если возраст человека равен" + ageMe + ", то ему");
+        if (ageMe > 2 && ageMe <= 6) {
+            System.out.println("нужно ходить в детский сад");
+        }
+        if (ageMe >= 7 && ageMe <= 17) {
+            System.out.println("Нужно ходить в школу");
+        }
+        if (ageMe >= 18 && ageMe <= 24) {
+            System.out.println("нужно ходить в университет");
+        }
+        if (ageMe > 24) {
+            System.out.println("нужно ходить на работу");
+        }
+        System.out.println("Задача 5");
+        int children = 12;
+        boolean older = false;
+        System.out.println("Если возраст ребенка равен"+children+", то ему");
+        System.out.println("");
+        if (children < 5) {
+            System.out.println("то он не может кататься на аттракционе.");
+        } else if (children < 14) {
+            if (older){
+                System.out.println("Кататься можно");
+            } else {
+                System.out.println("кататься нельзя");
+            }
+        } else {
+            System.out.println("он может кататься без сопровождения взрослого");
+        }
+        System.out.println("Задача 6");
+        int humanInTrain = 74;
+        int seatPlace = 60;
+        int plase =102;
+        if(humanInTrain>seatPlace || humanInTrain<plase){
+            System.out.println("Сидячих мест нет, но есть стоячие");
+        }else {
+            System.out.println("мест нет");
+        }
+        System.out.println("Задача 7");
+        int one =1;
+        int two = 2;
+        int three =3;
+        boolean oneIsBiggest = one>two && one>three;
+        boolean twoIsBiggest = two>one && two>three;
+        boolean threeIsBiggest = three>one && three>two;
+        if (oneIsBiggest) {
+            System.out.println("Число один - наибольшее из всех");
+        } else if (twoIsBiggest) {
+            System.out.println("Число два - наибольшее из всех");
+        }else {
+            System.out.println("Число три - наибольшее из всех");
+        }
+    }}
